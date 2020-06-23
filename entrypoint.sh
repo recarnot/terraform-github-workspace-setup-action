5a623dab4a60eb9e6d8cf16202ab7515168f57c3
ROMAIN ECARNOT <romain.ecarnot@open-groupe.com>
2020-06-23 13:49:12 +0200

    test14

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,11 @@
 
 echo "Organization $1"
 echo "Workspace $2"
-echo "Token $3"
 
 sed "s/T_WS/$2/" < ./template/workspace.payload.template > workspace.json
 curl --header "Authorization: Bearer $3" --header "Content-Type: application/vnd.api+json" --request POST --data @workspace.json "https://app.terraform.io/api/v2/organizations/$1/workspaces" > workspace_result
 wid=$(cat workspace_result | jq -r .data.id)
-
 wid=$(curl -s --header "Authorization: Bearer $3" --header "Content-Type: application/vnd.api+json" "https://app.terraform.io/api/v2/organizations/$1/workspaces/$2" | jq -r .data.id)
-
-
-cat workspace_result
 
 echo "Workspace ID $wid"
 echo "::set-output name=workspace_id::$wid"
