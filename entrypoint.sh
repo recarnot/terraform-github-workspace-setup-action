a9a2efbe57414268f8e7b296cd8106cc47a40144
ROMAIN ECARNOT <romain.ecarnot@open-groupe.com>
2020-06-23 13:21:55 +0200

    test2

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,6 @@ wid=$(cat workspace_result | jq -r .data.id)
 wid=$(curl -s --header "Authorization: Bearer $3" --header "Content-Type: application/vnd.api+json" "https://app.terraform.io/api/v2/organizations/$1/workspaces/$2" | jq -r .data.id)
 
 
-
+echo "Workspace ID $wid"
 echo "::set-output name=workspace_id::$wid"
 
