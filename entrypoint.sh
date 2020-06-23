9c7af71179d1a9900e219564b4ca9c6e22466157
ROMAIN ECARNOT <romain.ecarnot@open-groupe.com>
2020-06-23 13:51:01 +0200

    test16

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ echo "Workspace $2"
 sed "s/T_WS/$2/" < ./template/workspace.payload.template > workspace.json
 curl --header "Authorization: Bearer $3" --header "Content-Type: application/vnd.api+json" --request POST --data @workspace.json "https://app.terraform.io/api/v2/organizations/$1/workspaces" > workspace_result
 wid=$(cat workspace_result | jq -r .data.id)
-
 wid=$(curl -s --header "Authorization: Bearer $3" --header "Content-Type: application/vnd.api+json" "https://app.terraform.io/api/v2/organizations/$1/workspaces/$2" | jq -r .data.id)
 
 curl --header "Authorization: Bearer $3" --header "Content-Type: application/vnd.api+json" "https://app.terraform.io/api/v2/vars?filter%5Borganization%5D%5Bname%5D=$1&filter%5Bworkspace%5D%5Bname%5D=$2" > vars.json
@@ -17,6 +16,5 @@ do
   curl --header "Authorization: Bearer $1" --header "Content-Type: application/vnd.api+json" --request DELETE https://app.terraform.io/api/v2/vars/$(cat vars.json | jq -r ".data[$i].id")
 done
 
-echo "Workspace ID $wid"
 echo "::set-output name=workspace_id::$wid"
 
