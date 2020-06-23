3791397c8803eb0d5e5443c28fea602741bc41a6
ROMAIN ECARNOT <romain.ecarnot@open-groupe.com>
2020-06-23 13:16:57 +0200

    fix install

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM alpine:3.10
 
 COPY entrypoint.sh /entrypoint.sh
+COPY ./template .
+
+RUN apk update && \
+    apk add curl jq
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,11 @@ echo "Organization $1"
 echo "Workspace $2"
 
 
-sed "s/T_WS/$2/" < ./scripts/workspace.payload.template > workspace.json
+sed "s/T_WS/$2/" < ./template/workspace.payload.template > workspace.json
 curl --header "Authorization: Bearer $3" --header "Content-Type: application/vnd.api+json" --request POST --data @workspace.json "https://app.terraform.io/api/v2/organizations/$1/workspaces" > workspace_result
 wid=$(cat workspace_result | jq -r .data.id)
 
 wid=$(curl -s --header "Authorization: Bearer $3" --header "Content-Type: application/vnd.api+json" "https://app.terraform.io/api/v2/organizations/$1/workspaces/$2" | jq -r .data.id)
-#echo ::set-env name=TF_WORKSPACE_ID::$wid
-
-sed -e "s/T_ORGA/$1/" -e "s/T_WS/$2/" < ./scripts/backend.template > backend.tf
 
 curl --header "Authorization: Bearer $3" --header "Content-Type: application/vnd.api+json" "https://app.terraform.io/api/v2/vars?filter%5Borganization%5D%5Bname%5D=$1&filter%5Bworkspace%5D%5Bname%5D=$2" > vars.json
 x=$(cat vars.json | jq -r ".data[].id" | wc -l | awk '{print $1}')
