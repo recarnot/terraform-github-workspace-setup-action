10efb513d1c244dc4bd03516fc18dcab7875a2a1
ROMAIN ECARNOT <romain.ecarnot@open-groupe.com>
2020-06-24 12:21:45 +0200

    Test adding a variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,15 @@
 echo "Terraform Organization: $1"
 echo "Terraform Workspace: $2"
 
-sed "s/T_WS/$2/" < ./template/workspace.payload.template > workspace.json
+sed "s/T_WS/$2/" < ./template/workspace.payload > workspace.json
 curl --header "Authorization: Bearer $3" --header "Content-Type: application/vnd.api+json" --request POST --data @workspace.json "https://app.terraform.io/api/v2/organizations/$1/workspaces" > workspace_result
-#wid=$(cat workspace_result | jq -r .data.id)
 
-#If already created, retreive ID
 wid=$(curl -s --header "Authorization: Bearer $3" --header "Content-Type: application/vnd.api+json" "https://app.terraform.io/api/v2/organizations/$1/workspaces/$2" | jq -r .data.id)
 
 echo "::set-output name=workspace_id::$wid"
+
+
+#ESCAPED_VALUE=$(echo $2 | sed -e 's/[]\/$*.^[]/\\&/g');
+
+sed -e "s/T_KEY/my-key/" -e "s/my-hcl/false/" -e "s/T_VALUE/romain/" -e "s/T_SECURED/false/" -e "s/T_WSID/$wid/" < ./template/variable.payload  > variable.json
+curl --header "Authorization: Bearer $3" --header "Content-Type: application/vnd.api+json" --data @variable.json "https://app.terraform.io/api/v2/vars"
